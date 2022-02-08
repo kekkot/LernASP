5869e0d1cd229dafbeea155615615a55d5fcc0f4
kekkot <kotmarchic@gmail.com>
2022-02-08 21:57:34 +0300

    Добавил html страницу, пока ниче не работает

--- LernASP/Program.cs
+++ LernASP/Program.cs
@@ -4,6 +4,7 @@ using LernASP.data.mocks;
 var builder = WebApplication.CreateBuilder(args);// Билдер настраивает всю конфигурацию приложения
 builder.Services.AddTransient<IAllDetails, MockDetails>();//Интерфейс IAllDetails реализуется в MockDetails
 builder.Services.AddTransient<IdetailsCategory, MockCategory>();//Интерфейс IdetailsCategory реализуется в MockCategory
+builder.Services.AddMvc(option => option.EnableEndpointRouting =false);
 var app = builder.Build(); //Создаем объект WebApplication
 
 // Configure the HTTP request pipeline.
@@ -19,7 +20,11 @@ app.UseStatusCodePages(); //Отобразить код страницу
 //app.UseMvcWithDefaultRoute(); //Много всего, к примеру отслеживание URL адреса
 app.UseStaticFiles(); //Использование статических файлов
 
-
+app.UseRouting();//Пока хз что это, чисто из комментов на ютубе, чтоб работало
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(name: "default", pattern: "{controller = Detail}/{action = List}");
+});
 
 app.MapGet("/", () => "Hello World!");//MapGet первым параметром принимет путь, по которому обратится приложение
 //Вторым параметром принимает обработчик запроса по этому маршруту в виде функции
